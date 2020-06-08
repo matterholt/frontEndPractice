bfe389e9201b67214ea4e80a6b93728ad78767a8
MAtterholt <matterholt84@gmail.com>
2020-06-07 22:51:55 -0400

    gulp styles

--- landing_page/gulpfile.js
+++ landing_page/gulpfile.js
@@ -1,17 +1,26 @@
 const { src, dest, watch, parallel, series } = require("gulp");
 
 const pug = require("gulp-pug");
+const sass = require("gulp-sass");
 const rename = require("gulp-rename");
 
 function html_gen(cb) {
-  src("landing_page/src_view/*.pug")
+  src("src_view/*.pug")
     .pipe(rename({ extname: ".html" }))
-    .pipe(pug())
-    .pipe(dest("landing_page/public"));
+    .pipe(pug({ pretty: true }))
+    .pipe(dest("public"));
   cb();
 }
 
+function gen_css(cb) {
+  src("src_view/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("public/stylesheets"));
+  cb();
+}
+
+exports.css = gen_css;
 exports.html = html_gen;
 
 // exports.default = series(parallel(html_gen));
-exports.default = series(html_gen);
+exports.default = series(parallel(html_gen, gen_css));
